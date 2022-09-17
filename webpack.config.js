d9e9eb40282aa9b383cd504183a144e2a13cd93e
Mahdi <mahdihs395@gmail.com>
2022-09-17 11:53:04 +0300

    created basic html structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,24 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  devserver:{
-    static: './dist'
-  },
-  optimization: {
-    runtimeChunk: 'single',
+  devServer: {
+    static: './dist',
   },
   entry: './src/main.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: 'asset/resource'
-      }
-    ]
-  }
-}
+        use: 'asset/resource',
+      },
+    ],
+  },
+};
